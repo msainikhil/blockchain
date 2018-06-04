de8383ce258853945ae26b27f6aefe2f74f8a74b
mnnikh <mnnikh@amazon.com>
2018-06-04 23:10:52 +0530

    created a blockchain class

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # blockchain
 creating a simple blockchain implementation
+source: https://www.udemy.com/build-your-blockchain-az
--- create_blockchain/blockchain.py
+++ create_blockchain/blockchain.py
@@ -1,1 +1,17 @@
 # module 1 create blockchain
+# source : https://www.udemy.com/build-your-blockchain-az
+
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Building a blockchain
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+
+
+#Mining a Block
